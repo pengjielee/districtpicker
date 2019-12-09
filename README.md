36f1d119efc6caaf1b64936a225873152c2aa5bb
pengjielee <386276251@qq.com>
2019-12-09 17:48:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+## 省市区选择器
+
+- 默认，省/市/区；  
+- 只选择省；   
+- 隐藏区；  
+- 数据回填；  
+
+## 脚本  
+```
+## Project setup
+yarn install
+
+## Compiles and hot-reloads for development
+yarn run serve
+
+## Compiles and minifies for production
+yarn run build
+
+## Run your tests
+yarn run test
+
+## Lints and fixes files
+yarn run lint
+
+## Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## build lib
+yarn run build:lib
+```
+
+
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <h2>默认</h2>
+      <district-picker v-on:selected="handleSelected"></district-picker>
+    </div>
+    <div class="card">
+      <h2>只显示省</h2>
+      <district-picker :onlyProvince="onlyProvince" v-on:selected="handleSelected"></district-picker>
+    </div>
+    <div class="card">
+      <h2>隐藏区</h2>
+      <district-picker :hideArea="hideArea" v-on:selected="handleSelected"></district-picker>
+    </div>
+    <div class="card">
+      <h2>数据回填</h2>
+      <district-picker province="北京" city="市辖区" area="朝阳区" v-on:selected="handleSelected"></district-picker>
+    </div>
+  </div>
+</template>
+
+<script>
+import DistrictPicker from "./DistrictPicker.vue"
+
+export default {
+  components: {
+    DistrictPicker
+  },
+  methods: {
+    handleSelected(value){
+      /* eslint-disable no-console */
+      console.log(value);
+    }
+  },
+  data() {
+    return {
+      title: "",
+      onlyProvince: true,
+      hideArea: false
+    };
+  }
+};
+</script>
